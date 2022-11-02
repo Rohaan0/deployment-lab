c61adca48cedfac31ac35198fbd0536a08c802ea
Rohaan Anderson <Rohaan.a.anderson16@gmail.com>
2022-11-02 14:21:07 -0600

    deployment-lab

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config
+const path = require('path')
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+const { home } = require("./controllers/controller.js")
+
+app.get("/", home)
+
+const { PORT } = process.env
+
+console.log(__dirname)
+
+app.listen(PORT, () => console.log( `server listening at port ${PORT}`))
